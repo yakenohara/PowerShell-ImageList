80725939309a5310a897275b9996a13a09b9f7c2
yakenohara <38835246+yakenohara@users.noreply.github.com>
2021-04-26 16:55:05 +0900

    1st

---
+++ HoistImages.bat
@@ -0,0 +1,27 @@
+::<License>------------------------------------------------------------
+::
+:: Copyright (c) 2021 Shinnosuke Yakenohara
+::
+:: This program is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU General Public License as published by
+:: the Free Software Foundation, either version 3 of the License, or
+:: (at your option) any later version.
+::
+:: This program is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+:: GNU General Public License for more details.
+::
+:: You should have received a copy of the GNU General Public License
+:: along with this program.  If not, see <http://www.gnu.org/licenses/>.
+::
+::-----------------------------------------------------------</License>
+
+::定数
+set ps1FileName=HoistImages.ps1
+
+::初期化
+set ps1FileFullPath=%~dp0%ps1FileName%
+
+::Call powershell
+powershell -ExecutionPolicy Bypass "& \"%ps1FileFullPath%\""
---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+# What's this?
+
+Make markdown file that represents all image file list. ( `*.png`, `*.jpg`, `*.jpeg`, `*.svg`)
+
+# How to Use
+
+Place `HoistImages.bat` and `HoistImages.ps1` to specified ditectory.  
+`HoistImages.bat`
